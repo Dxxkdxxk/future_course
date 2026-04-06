8625d497ad4fb6c7f5621395a8179d7bdd7483ae
Zheng Liu <92808182+lz2021211161@users.noreply.github.com>
2026-04-07 00:45:18 +0800

    Update StudentExcelDTO.java

--- src/main/java/com/lzlz/springboot/security/entity/StudentExcelDTO.java
+++ src/main/java/com/lzlz/springboot/security/entity/StudentExcelDTO.java
@@ -6,15 +6,11 @@ import lombok.Data;
 @Data
 public class StudentExcelDTO {
 
-    @ExcelProperty("班级")
     private String className;
 
-    @ExcelProperty("姓名")
     private String studentName;
 
-    @ExcelProperty("用户名")
     private String username;
 
-    @ExcelProperty("初始密码")
     private String password;
 }
