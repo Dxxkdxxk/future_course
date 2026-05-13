888cc4e16041abc29238594a21c52bd34c710e73
Zheng Liu <92808182+lz2021211161@users.noreply.github.com>
2026-05-13 09:26:20 +0800

    Update VideoPlayProgressService.java

--- src/main/java/com/lzlz/springboot/security/service/VideoPlayProgressService.java
+++ src/main/java/com/lzlz/springboot/security/service/VideoPlayProgressService.java
@@ -3,9 +3,9 @@ package com.lzlz.springboot.security.service;
 import com.lzlz.springboot.security.dto.SaveVideoProgressDTO;
 
 public interface VideoPlayProgressService {
-    // 保存进度
-    void saveOrUpdateProgress(Long userId, SaveVideoProgressDTO dto);
+    // 保存/更新：增加 courseId
+    void saveOrUpdateProgress(Long userId, Long courseId, SaveVideoProgressDTO dto);
 
-    // 查询进度：第二个参数 改为 String resourceId（和实现类、前端对应）
-    Integer getProgress(Long userId, String resourceId);
+    // 查询：增加 courseId
+    Integer getProgress(Long userId, Long courseId, String resourceId);
 }
